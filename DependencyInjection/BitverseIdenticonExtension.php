2a68fdd21f47e9b734acfbb006a712d84e19370c
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-10-14 08:34:52 +0200

    Updated BitverseIdenticonExtension

--- DependencyInjection/BitverseIdenticonExtension.php
+++ DependencyInjection/BitverseIdenticonExtension.php
@@ -2,6 +2,7 @@
 
 namespace Bitverse\IdenticonBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -11,19 +12,20 @@ class BitverseIdenticonExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
+        $processor = new Processor();
         $configuration = new Configuration();
 
-        $config = $this->precessConfiguration($configuration, $configs);
+        $config = $processor->processConfiguration($configuration, $configs);
 
         $loader = new XMLFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../Resources/config')
         );
 
+        $loader->load('config.xml');
+
         foreach (['preprocessor', 'generator'] as $key) {
             $container->setParameter('identicon.' . $key, $config[$key]);
         }
-
-        $loader->load('config.xml');
     }
 }
