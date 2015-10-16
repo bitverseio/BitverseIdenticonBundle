3a2c7621df13fcf1d1339ffe55ca03a976152e6a
Kuba Birecki <kuba.birecki@crystalplanet-studio.com>
2015-10-16 09:12:00 +0200

    Switched from XML to YAML config and updated the extension class

--- DependencyInjection/BitverseIdenticonExtension.php
+++ DependencyInjection/BitverseIdenticonExtension.php
@@ -4,8 +4,8 @@ namespace Bitverse\IdenticonBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
 class BitverseIdenticonExtension extends Extension
@@ -14,18 +14,28 @@ class BitverseIdenticonExtension extends Extension
     {
         $processor = new Processor();
         $configuration = new Configuration();
-
         $config = $processor->processConfiguration($configuration, $configs);
 
-        $loader = new XMLFileLoader(
+        $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../Resources/config')
         );
 
-        $loader->load('config.xml');
+        $loader->load('config.yml');
 
-        foreach (['preprocessor', 'generator'] as $key) {
-            $container->setParameter('identicon.' . $key, $config[$key]);
-        }
+        $container->setParameter(
+            'bitverse_identicon.preprocessor.class',
+            $config['preprocessor']['class']
+        );
+
+        $container->setParameter(
+            'bitverse_identicon.generator.class',
+            $config['generator']['class']
+        );
+
+        $container->setParameter(
+            'bitverse_identicon.generator.background_color',
+            $config['generator']['background_color']
+        );
     }
 }
